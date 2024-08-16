a359261995fd0d53b547d0f077a86aaa48fe755f
typeling1578 <pub@typeling1578.dev>
2024-08-16 17:10:41 +0900

    fix path

--- src/components/routers/top.ts
+++ src/components/routers/top.ts
@@ -3,7 +3,7 @@ import fastify from "fastify";
 import fs from "fs";
 
 export default async function topRouter(req: fastify.FastifyRequest, res: fastify.FastifyReply) {
-    const response = await fs.promises.readFile(`${appRootPath.path}/src/components/top-page/page.html`);
+    const response = await fs.promises.readFile(`${appRootPath.path}/built/components/top-page/page.html`);
     res.status(200);
     res.type("text/html");
     return res.send(response);
